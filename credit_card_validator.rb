aafd5d38e1c5b403df7e9d3be32488f4495cbb17
Philipe Godoy <philipe@sorrydave.com.br>
2014-12-08 21:42:28 -0300

    first commit

---
+++ credit_card_validator.rb
@@ -0,0 +1,39 @@
+def is_valid (credit_number)
+  if credit_number.size == 16
+  	last_number = credit_number[15].to_i
+  	credit_number[15] = ''
+  	credit_number.reverse!  	
+  	total = 0
+  	i = 0
+  	credit_number.each_char do |number|  		
+  		number = number.to_i  		
+  		if i.even?
+  			
+  			number = number * 2
+  			if number > 9
+  				number = number - 9  	  				
+  			end
+  			total += number
+  		else
+			total += number
+  		end
+  		i += 1
+  		print "Total: #{total}\n"
+  	end  	    
+  	if ((10 - (total % 10)) == last_number)
+  		true
+  	else
+  		false
+  	end
+  else
+  	false
+  end
+end
+
+print "Enter the credit card number"
+credit_number = gets.strip.to_s
+if is_valid(credit_number)
+  print "valid credit card"
+else
+  print "invalid credit card"
+end
